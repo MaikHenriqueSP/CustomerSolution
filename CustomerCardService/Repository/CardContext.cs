5815a663994b6fcc173a33a21d71c4eef964ca14
Maik Pereira <maik.pereira@capgemini.com>
2021-06-23 15:26:22 -0300

    feat(repository): addition of in memory database with Entity Framework
    - registering DI injection at Startup.cs
    - implementation of the DbContext configuration for the Card model

---
+++ CustomerCardService/Repository/CardContext.cs
@@ -0,0 +1,16 @@
+﻿using CustomerCardService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerCardService.Repository
+{
+    public class CardContext : DbContext
+    {
+        public CardContext(DbContextOptions<CardContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
