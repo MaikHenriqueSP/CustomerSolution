f1d653c377f64a190b2e89a514114d59c6315aa9
Maik Pereira <maik.pereira@capgemini.com>
2021-06-23 15:10:00 -0300

    feat(card): creation of the Card model

---
+++ CustomerCardService/Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerCardService.Models
+{
+    public class Card
+    {
+        private Guid Token { get; set; }
+        private Guid CardId { get; }
+        private int CustomerId { get; }
+        public long CardNumber { get; }
+        public int CVV { get; }
+
+        public Card(int customerId, long cardNumber, int cVV)
+        {
+            CustomerId = customerId;
+            CardNumber = cardNumber;
+            CVV = cVV;
+        }
+    }
+}
