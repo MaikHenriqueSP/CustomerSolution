4941622daf317320456518966469d86616da0479
Maik Pereira <maik.pereira@capgemini.com>
2021-06-23 17:53:27 -0300

    feat(api): creation of models folder for Input and Output DTOs of the Card model

---
+++ CustomerCardService/Api/Models/Output/CardOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerCardService.Api.Models.Output
+{
+    public class CardOutput
+    {
+        public int CustomerId { get; }
+        public Guid CardId { get; }
+        public Guid Token { get; }
+        public int CVV { get; }
+    }
+}
