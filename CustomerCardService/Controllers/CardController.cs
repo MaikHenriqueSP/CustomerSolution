aeaec9a5b7709d4ff90c972ad737823a541fcdc3
Maik Pereira <maik.pereira@capgemini.com>
2021-06-23 15:34:51 -0300

    feat(controller): creation of the CardController
    - addition of the Controllers folder to the .csproj configuration file

---
+++ CustomerCardService/Controllers/CardController.cs
@@ -0,0 +1,23 @@
+﻿using CustomerCardService.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerCardService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardController : ControllerBase
+    {
+        private readonly CardContext _cardContext;
+
+        public CardController(CardContext cardContext)
+        {
+            _cardContext = cardContext;
+        }
+
+
+    }
+}
