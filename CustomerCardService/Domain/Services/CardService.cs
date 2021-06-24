c52ef13c6440433101e5eb63e58811f48dc8d50f
Maik Pereira <maik.pereira@capgemini.com>
2021-06-24 14:38:50 -0300

    feat(token): implementation of the token generation algorithm
    - creation of auxiliary methods to support the generation and improve readability

--- CustomerCardService/Domain/Services/CardService.cs
+++ CustomerCardService/Domain/Services/CardService.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CustomerCardService.Domain.Services
@@ -24,7 +26,14 @@ namespace CustomerCardService.Domain.Services
 
         public Guid GenerateToken(CardSaveInput card)
         {
-            throw new NotImplementedException();
+            long cardNumber = card.CardNumber;
+            int cardNumberLastFourDigits = GetLastFourDigits(cardNumber);
+            int[] rightRotatedDigits = RightRotateNumberToIntArray(cardNumberLastFourDigits, card.CVV);
+            string rotatedString = string.Join("", rightRotatedDigits);
+            byte[] hashedBytes = CalculateStringMD5Hash(rotatedString);
+            string hashedRotatedString = ConvertByteArrayToString(hashedBytes);
+
+            return new Guid(hashedRotatedString);
         }
 
         public CardSaveOutput SaveCard(CardSaveInput cardInput)
@@ -34,15 +43,16 @@ namespace CustomerCardService.Domain.Services
 
             if (cardOrDefault == null)
             {
-                //Guid token = GenerateToken(cardInput);
                 Card card = mapper.Map<Card>(cardInput);
+                card.Token = GenerateToken(cardInput);
+
                 cardContext.AddAsync(card);
                 cardContext.SaveChangesAsync();
+
                 return mapper.Map<CardSaveOutput>(card);
             }
 
-            //@TODO: Validate the card input against the one found
-            cardOrDefault.Token = System.Guid.NewGuid();
+            cardOrDefault.Token = GenerateToken(cardInput);
             cardContext.AddAsync(cardOrDefault);
             cardContext.SaveChangesAsync();
 
@@ -54,5 +64,46 @@ namespace CustomerCardService.Domain.Services
         {
             throw new NotImplementedException();
         }
+
+        private static int GetLastFourDigits(long number)
+        {
+            return (int)(number % 10000);
+        }
+
+        private byte[] CalculateStringMD5Hash(string target)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                return md5.ComputeHash(Encoding.UTF8.GetBytes(target));
+            }
+        }
+
+        private string ConvertByteArrayToString(byte[] hash)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+
+        private int[] RightRotateNumberToIntArray(int number, int rotations)
+        {
+            int numberLength = (int)Math.Log10(number) + 1;
+            int[] rightRotatedVector = new int[numberLength];
+
+            for (int i = numberLength - 1, multiplicationFactor = 10, divisionFactor = 1; i >= 0; i--,
+                multiplicationFactor *= 10, divisionFactor *= 10)
+            {
+                int digit = (number % multiplicationFactor) / divisionFactor;
+                rightRotatedVector[(i + rotations) % numberLength] = digit;
+            }
+            return rightRotatedVector;
+        }
+
+
     }
 }
