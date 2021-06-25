db35728c387bf420ab499283c6ef4dc62c0601f1
Maik Pereira <maik.pereira@capgemini.com>
2021-06-25 12:20:20 -0300

    feat: implementation of success case of PostCard request

---
+++ CustomerCardService.IntegrationTests/CardControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using CustomerCardService.Api.Models.Input;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CustomerCardService.IntegrationTests
+{
+    public class CardControllerTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task PostCard_WhenCardIsValid_ReturnsStatusCodeCreated()
+        {
+            //Arrange
+            var validaCard = new CardSaveInput()
+            {
+                CVV = 2313,
+                CardNumber = 2313,
+                CustomerId = 31
+            };
+
+            //Act
+            string endpoint = "/api/v1/Card";
+            string mediaType = "application/json";
+            var response = await TestClient.PostAsync(endpoint,
+                new StringContent(
+                    JsonConvert.SerializeObject(validaCard), Encoding.UTF8,
+                    mediaType));
+
+            response.EnsureSuccessStatusCode();
+
+            //Assert
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+    }
+}
