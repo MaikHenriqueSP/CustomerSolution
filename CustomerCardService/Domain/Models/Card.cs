a9f6f370c68e7bdd0569d1a4ef4c36e6201b1c58
Maik Pereira <maik.pereira@capgemini.com>
2021-06-27 18:22:41 -0300

    refactor(card): addition of field validation for CVV and CardNumber
    - CVV can't be longer than 5 digits or else it throws an exception
    - CardNumber can't be longer than 16 digits or else it throws an exception

--- CustomerCardService/Domain/Models/Card.cs
+++ CustomerCardService/Domain/Models/Card.cs
@@ -1,11 +1,7 @@
-﻿using CustomerCardService.Core.Validation;
-using Microsoft.EntityFrameworkCore;
+﻿using CustomerCardService.Core.Utilities;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CustomerCardService.Domain.Models
 {
@@ -21,13 +17,44 @@ namespace CustomerCardService.Domain.Models
 
         [Required]
         public long CardNumber { get; set; }
-        
+
         [Required]
         public int CVV { get; set; }
 
         public Guid Token { get; set; }
 
         public DateTimeOffset TokenCreationDate { get; set; }
+
+        public void SetCVV(int cVV)
+        {
+            if (!ValidationUtilities.IsNumberWithinMaxNumberOfDigits(cVV, 5))
+            {
+                throw new ArgumentException("The CVV shouldn't have more than 5 digits");
+            }
+
+            if (cVV < 0)
+            {
+                throw new ArgumentException("The CVV shouldn't be negative");
+            }
+
+            CVV = cVV;
+        }
+
+        public void SetCardNumber(int cardNumber)
+        {
+            if (!ValidationUtilities.IsNumberWithinMaxNumberOfDigits(cardNumber, 16))
+            {
+                throw new ArgumentException("The Card Number shouldn't have more than 16 digits");
+            }
+
+            if (cardNumber < 0)
+            {
+                throw new ArgumentException("The CVV shouldn't be negative");
+            }
+
+            CardNumber = cardNumber;
+        }
+
 
     }
 }
